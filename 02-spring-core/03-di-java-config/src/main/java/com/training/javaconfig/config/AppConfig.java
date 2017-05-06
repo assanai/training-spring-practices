d85a1066c03de5d80b0997c0f7b03a186bc09c9f
Assanai Manurat <assanai17@gmail.com>
2017-05-07 00:39:35 +0700

    Add classes for using scope [single, prototype]

--- 02-spring-core/03-di-java-config/src/main/java/com/training/javaconfig/config/AppConfig.java
+++ 02-spring-core/03-di-java-config/src/main/java/com/training/javaconfig/config/AppConfig.java
@@ -2,14 +2,13 @@ package com.training.javaconfig.config;
 
 import com.training.javaconfig.bean.EmployeeBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 
-/**
- * Created by amanurat on 3/15/2016 AD.
- */
+
 @Configuration
-@ImportResource(locations = "classpath:spring-config.xml")
+@ComponentScan(value = "com.training.javaconfig")
+//@ImportResource(locations = "classpath:spring-config.xml")
 public class AppConfig {
 
     @Bean
